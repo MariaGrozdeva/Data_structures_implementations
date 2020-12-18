f2f8381036ceb4c78f97bd871eaccefa806db0e4
Maria Grozdeva <mggrozdeva@gmail.com>
2020-12-18 14:22:12 +0200

    update BST

--- Binary search tree/main.cpp
+++ Binary search tree/main.cpp
@@ -4,13 +4,17 @@ int main()
 {
 	BinarySearchTree<int> bst;
 	bst.insert(1);
-	bst.insert(5);
 	bst.insert(3);
-
+	bst.insert(5);
 	bst.print();
 	cout << endl;
 
-	vector<int> vc{ 1,3,5,7,9 };
-	BinarySearchTree<int>(vc);
+	bst.remove(1);
 	bst.print();
+	cout << endl;
+
+	vector<int> v{ 1,3,5,7,9 };
+
+	BinarySearchTree<int> bst2(v);
+	bst2.print();
 }
