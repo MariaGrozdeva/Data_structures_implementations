5a2cb54734256c18dffefeed6ea5026c52616b72
Maria Grozdeva <mggrozdeva@gmail.com>
2021-01-09 15:53:59 +0200

    added Prim

--- Graphs/algorithms/Algorithms.h
+++ Graphs/algorithms/Algorithms.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <queue>
 #include <stack>
 #include <functional>
@@ -17,19 +16,3 @@ bool connected(const Graph& g);
 int Dijkstra(const Graph& g, int start, int end, vector<int>& path);
 
 int Prim(const Graph& g, Graph& mst);
-=======
-#include <queue>
-#include <stack>
-#include "Graph.h"
-
-void BFS(const Graph& g, int start);
-void DFS(const Graph& g, int start);
-
-bool BFS_containsPath(const Graph& g, int start, int end);
-bool containsPath(const Graph& g, int start, int end);
-
-bool BFS_connected(const Graph& g);
-bool connected(const Graph& g);
-
-int Dijkstra(const Graph& g, int start, int end, vector<int>& path);
->>>>>>> 53aed6a93650d8aa658aa387583e956af5645eb6
