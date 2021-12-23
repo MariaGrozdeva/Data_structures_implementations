8a1ddee85b9abefee01c73ebc6a887578216121a
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-12-23 13:59:03 +0200

    Update TasksOnTreesAndGraphs.cpp

--- Tasks on binary trees and graphs/TasksOnTreesAndGraphs.cpp
+++ Tasks on binary trees and graphs/TasksOnTreesAndGraphs.cpp
@@ -13,7 +13,7 @@ struct Node
 	Node* left;
 	Node* right;
 
-	Node(T data) : data(data), left(nullptr), right(nullptr) {}
+	Node(const T& data) : data(data), left(nullptr), right(nullptr) {}
 };
 
 template <typename T>
