da96ac071e5905ffaccb860e7d6290fc5ce2eee2
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2022-01-11 16:28:18 +0200

    Update BinaryTree.cpp

--- Tasks on trees/binaryTree/BinaryTree.cpp
+++ Tasks on trees/binaryTree/BinaryTree.cpp
@@ -14,6 +14,10 @@ struct Node
 
 	Node(const T& data) : data(data), left(nullptr), right(nullptr) {}
 };
+bool comp(Node<int>* lhs, Node<int>* rhs) 
+{
+	return lhs->data < rhs->data;
+}
 
 template <typename T>
 void print(Node<T>* root)
@@ -416,39 +420,39 @@ int sumOfNodesWithOddNumberOfChildren(Node<int>* root, int start, int end)
 // и проверява дали е вярно, че елементите на всяко ниво L на едното дърво съвпадат с елементите на съответното ниво L на другото дърво.
 bool areVectorsSame(vector<Node<int>*> nodes1, vector<Node<int>*> nodes2)
 {
-	for (int j = 0; j < nodes1.size(); j++)
+	sort(nodes1.begin(), nodes1.end(), comp);
+	sort(nodes2.begin(), nodes2.end(), comp);
+
+	for (size_t i = 0; i < nodes1.size(); i++)
 	{
-		bool isCorrect = false;
-		for (int k = 0; k < nodes1.size(); k++)
-		{
-			if (nodes1[j]->data == nodes2[k]->data)
-			{
-				isCorrect = true;
-				break;
-			}
-		}
-		if (!isCorrect)
+		if (nodes1[i]->data != nodes2[i]->data)
 			return false;
 	}
 
 	return true;
 }
-void createNextVectors(vector<Node<int>*>& nodes1, vector<Node<int>*>& nodes2, vector<Node<int>*>& nodes1Next, vector<Node<int>*>& nodes2Next)
+bool createNextVectors(vector<Node<int>*>& nodes1, vector<Node<int>*>& nodes2)
 {
-	for (int j = 0; j < nodes1.size(); j++)
-	{
-		if (nodes1[j]->left)
-			nodes1Next.push_back(nodes1[j]->left);
-		if (nodes1[j]->right)
-			nodes1Next.push_back(nodes1[j]->right);
+	vector<Node<int>*> nodes1Next;
+	vector<Node<int>*> nodes2Next;
 
-		if (nodes2[j]->left)
-			nodes2Next.push_back(nodes2[j]->left);
-		if (nodes2[j]->right)
-			nodes2Next.push_back(nodes2[j]->right);
+	for (size_t i = 0; i < nodes1.size(); i++)
+	{
+		if (nodes1[i]->left)
+			nodes1Next.push_back(nodes1[i]->left);
+		if (nodes1[i]->right)
+			nodes1Next.push_back(nodes1[i]->right);
+
+		if (nodes2[i]->left)
+			nodes2Next.push_back(nodes2[i]->left);
+		if (nodes2[i]->right)
+			nodes2Next.push_back(nodes2[i]->right);
 	}
+
 	nodes1 = nodes1Next;
 	nodes2 = nodes2Next;
+
+	return nodes1.size() == nodes2.size();
 }
 bool sameOnLevel(Node<int>* root1, Node<int>* root2)
 {
@@ -459,28 +463,18 @@ bool sameOnLevel(Node<int>* root1, Node<int>* root2)
 
 	vector<Node<int>*> nodes1;
 	vector<Node<int>*> nodes2;
-	vector<Node<int>*> nodes1Next;
-	vector<Node<int>*> nodes2Next;
 
 	nodes1.push_back(root1);
 	nodes2.push_back(root2);
 
-	for (int i = 0; i <= height1; i++)
+	for (size_t i = 0; i <= height1; i++)
 	{
-		if (!areVectorsSame(nodes1, nodes2))
+		if (!areVectorsSame(nodes1, nodes2) || !createNextVectors(nodes1, nodes2))
 			return false;
-
-		createNextVectors(nodes1, nodes2, nodes1Next, nodes2Next);
-
-		if (nodes1.size() != nodes2.size())
-			return false;
-
-		while (!nodes1Next.empty())
-		{
-			nodes1Next.pop_back();
-			nodes2Next.pop_back();
-		}
 	}
+
+	free(root1);
+	free(root2);
 	return true;
 }
 
